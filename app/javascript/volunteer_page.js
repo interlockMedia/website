b9a66310b66ed7ecefc9f71e6ab2a99c036df092
“Yang <wang_yang@bentley.edu>
2017-08-01 20:01:04 -0400

    donate window added

--- app/javascript/volunteer_page.js
+++ app/javascript/volunteer_page.js
@@ -1,2 +1,18 @@
 import '../stylesheets/volunteer_page.css';
 import './common.js';
+//modal function
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//paypal input placeholder
+$( "#myMessage" ).attr('placeholder',$('.donate-now input[type="radio"]:checked').val());
+$( ".donate-now input[type=\"radio\"]" ).click(function() {
+    $( "#myMessage" ).attr('placeholder',$(this).val());
+    $( "#myMessage" ).attr('value',$(this).val());
+});
